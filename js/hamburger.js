29798b75835f26c43f0569feefe1a9ff482aa526
SanthoshVelaga <santhosh@Santhoshs-MacBook-Pro.local>
2019-06-27 18:37:35 -0700

    this is the first commit

---
+++ js/hamburger.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	$('#nav-icon3').click(function(){
+    openNav();
+    addOverlay();
+
+		if (this.matches('.open')) {
+      closeNav();
+      removeOverlay()
+		}
+
+		$(this).toggleClass('open');
+	});
+});
+
+function openNav(){
+	document.getElementById("c").style.width = "100%";
+}
+
+function closeNav(){
+	document.getElementById("c").style.width = "0%";
+}
+
+function addOverlay(){
+  $(overlay).removeClass('overlay-content-hide');
+}
+
+function removeOverlay(){
+  $(overlay).addClass('overlay-content-hide');
+}
+
+
+
+
+
+
+
+
+
