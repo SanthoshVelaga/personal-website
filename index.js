b99e0c55f2c1058dfc7b495024d644cfc38f20bf
SanthoshVelaga <santhosh@Santhoshs-MacBook-Pro.local>
2019-07-01 02:25:10 -0700

    home, bio, and contact pages all made responsive with media tages

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const exphbs  = require('express-handlebars');
 const PORT = process.env.PORT || 5000
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', exphbs());
