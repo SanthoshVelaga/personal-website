cb143686626ba7865a771801b48ee4401ec3ab67
Tosh Velaga <toshvelaga@gmail.com>
2020-07-29 05:02:30 -0700

    got rid of compression

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const exphbs  = require('express-handlebars');
-const compression = require('compression')
-
-app.use(compression())
 
 const PORT = process.env.PORT || 5000
 
